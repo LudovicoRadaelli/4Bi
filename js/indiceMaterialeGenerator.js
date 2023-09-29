a5b8f4c19a5987d2bf60e6bece83e5f0fc4bd3c7
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2023-09-29 11:45:26 +0200

    Update indiceMaterialeGenerator.js

--- js/indiceMaterialeGenerator.js
+++ js/indiceMaterialeGenerator.js
@@ -3,7 +3,7 @@ fetch("capitoli.json")
   .then(jsonContent => {
 
     for(let chapterNumber = 1; chapterNumber <= jsonContent["capitolo"].length; chapterNumber++) {
-        let div = document.querySelectorAll(".chapter")[chapterNumber-1];
+        let div = document.querySelectorAll(".chapters")[chapterNumber-1];
         let h2 = document.createElement("h2");
         let nomeCartellaCapitolo =  jsonContent["capitolo"][chapterNumber -1]["nomeCartella"]
         h2.className = "chapter"
@@ -11,11 +11,11 @@ fetch("capitoli.json")
         div.appendChild(h2);
 
         for(let i = 0; i < jsonContent["capitolo"][chapterNumber -1]["paragrafo"].length; i++) {
-            let paragraphNumber = jsonContent["capitolo"][chapterNumber -1]["paragrafo"][i]["numero"];
+            let paragraphNumber = `${chapterNumber}.${i + 1}`;
             let paragraphTitle = jsonContent["capitolo"][chapterNumber -1]["paragrafo"][i]["titolo"];
             let nomeCartellaParagrafo = jsonContent["capitolo"][chapterNumber -1]["paragrafo"][i]["nomeCartella"]
             let a = document.createElement("a");
-            a.href = `${chapterNumber}${nomeCartellaCapitolo}/${nomeCartellaParagrafo}/index.html`;
+            a.href = `${nomeCartellaCapitolo}/${nomeCartellaParagrafo}/index.html`;
             a.innerHTML = `<code><b>${paragraphNumber}) ${paragraphTitle}</b></code><br><br>`
             div.appendChild(a);
         }
